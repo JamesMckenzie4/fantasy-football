b8cfce1b508406965bd4a6f29593f4f5a94ee6fb
James McKenzie <jm@risq.uk>
2020-03-04 17:55:12 +0000

    Refactor

--- code/python/scrape_fantasy_premier_league.py
+++ code/python/scrape_fantasy_premier_league.py
@@ -17,7 +17,8 @@ def get_player_data():
             player_data = list(player.values())
             player_data = [unidecode(str(i)) for i in player_data]
             write_file.write('{}\n'.format(','.join(player_data)))
-            
+
+
 def get_player_fixtures(n_players=489):
     """Write player fixture list to csv file"""
     with open('../../data/fantasy-premier-league-data/player-fixtures.csv', 'w') as write_file:
@@ -33,7 +34,8 @@ def get_player_fixtures(n_players=489):
                 fixture_data = [str(i) for i in fixture.values()]
                 write_file.write('{},{}\n'.format(i, ','.join(fixture_data)))
             time.sleep(1)
-            
+
+
 def get_player_types():
     """Get player type ID to position mapping"""
     with open('../../data/fantasy-premier-league-data/player-types.csv', 'w') as write_file:
@@ -45,7 +47,8 @@ def get_player_types():
         for player_type in player_types:
             player_type = [str(i) for i in player_type.values()]
             write_file.write('{}\n'.format(','.join(player_type)))
-            
+
+
 def get_team_data():
     """Get team ID to name mapping as well as some team statistics"""
     with open('../../data/fantasy-premier-league-data/teams.csv', 'w') as write_file:
@@ -61,11 +64,13 @@ def get_team_data():
             team = [str(i) for i in team.values()]
             write_file.write('{}\n'.format(','.join(team)))
             
+
 def main():
     get_player_data()
     #get_player_fixtures()
     #get_player_types()
     #get_team_data()
-    
+
+
 if __name__ == "__main__":
     main()
