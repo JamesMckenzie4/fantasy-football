6d9769dde43f1941baf2fda7ba6163792ec02b65
James McKenzie <jm@risq.uk>
2020-04-02 21:23:28 +0100

    Refactor code

--- code/python/scrape_fantasy_overlord.py
+++ code/python/scrape_fantasy_overlord.py
@@ -8,16 +8,60 @@ import requests
 from config import db_config
 from query_database import QueryDatabase
 
-URL = 'https://fantasyoverlord.com/FPL/History'
-SQL_DIR = '../../'
-COLUMNS = ['game_week', 'FirstName', 'Surname', 'PositionsList', 'Team', 'Cost', 'PointsLastRound', 'TotalPoints',
-           'AveragePoints', 'AveragePointsPerDollar', 'TotalPointsPerDollar', 'GameweekWeighting', 'TransfersOut',
-           'YellowCards', 'GoalsConceded', 'GoalsConcededPoints', 'Saves', 'SavesPoints', 'GoalsScored',
-           'GoalsScoredPoints', 'ValueSeason', 'TransfersOutRound', 'PriceRise', 'PriceFallRound', 'LastSeasonPoints',
-           'PriceFall', 'ValueForm', 'PenaltiesMissed', 'Form', 'Bonus', 'FanRating', 'CleanSheets', 'CleanSheetPoints',
-           'Assists', 'SelectedByPercent', 'TransfersIn', 'OwnGoals', 'EAIndex', 'PenaltiesSaved', 'DreamteamCount',
-           'MinutesPlayed', 'TransfersInRound', 'PriceRiseRound', 'RedCards', 'BPS', 'NextFixture1', 'NextFixture2',
-           'NextFixture3', 'NextFixture4', 'NextFixture5']
+URL = "https://fantasyoverlord.com/FPL/History"
+SQL_DIR = "../../"
+COLUMNS = [
+    "game_week",
+    "FirstName",
+    "Surname",
+    "PositionsList",
+    "Team",
+    "Cost",
+    "PointsLastRound",
+    "TotalPoints",
+    "AveragePoints",
+    "AveragePointsPerDollar",
+    "TotalPointsPerDollar",
+    "GameweekWeighting",
+    "TransfersOut",
+    "YellowCards",
+    "GoalsConceded",
+    "GoalsConcededPoints",
+    "Saves",
+    "SavesPoints",
+    "GoalsScored",
+    "GoalsScoredPoints",
+    "ValueSeason",
+    "TransfersOutRound",
+    "PriceRise",
+    "PriceFallRound",
+    "LastSeasonPoints",
+    "PriceFall",
+    "ValueForm",
+    "PenaltiesMissed",
+    "Form",
+    "Bonus",
+    "FanRating",
+    "CleanSheets",
+    "CleanSheetPoints",
+    "Assists",
+    "SelectedByPercent",
+    "TransfersIn",
+    "OwnGoals",
+    "EAIndex",
+    "PenaltiesSaved",
+    "DreamteamCount",
+    "MinutesPlayed",
+    "TransfersInRound",
+    "PriceRiseRound",
+    "RedCards",
+    "BPS",
+    "NextFixture1",
+    "NextFixture2",
+    "NextFixture3",
+    "NextFixture4",
+    "NextFixture5",
+]
 
 
 def scrape_fantasy_overlord(to_exclude=None):
@@ -36,29 +80,29 @@ def scrape_fantasy_overlord(to_exclude=None):
 
     data = []
     page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    url_start = URL.replace('/FPL/History', '')
-    for link in soup.findAll('a'):
-        if 'file' in link['href']:
+    soup = BeautifulSoup(page.content, "html.parser")
+    url_start = URL.replace("/FPL/History", "")
+    for link in soup.findAll("a"):
+        if "file" in link["href"]:
             # Get game week id from file name
-            file_id = link['href'].replace('/FPL/History?file=', ',').replace(',', '')
+            file_id = link["href"].replace("/FPL/History?file=", ",").replace(",", "")
             if to_exclude and file_id in to_exclude:
                 continue
-            print('Processing game week - {}'.format(file_id))
-            r = requests.get(url_start + link['href']).content
+            print("Processing game week - {}".format(file_id))
+            r = requests.get(url_start + link["href"]).content
             try:
-                df = pd.read_csv(io.StringIO(r.decode('utf-8')))
+                df = pd.read_csv(io.StringIO(r.decode("utf-8")))
             except (EmptyDataError, ParserError):
                 continue
-            df['game_week'] = file_id
+            df["game_week"] = file_id
             game_week_data = list(df[COLUMNS].itertuples(index=False, name=None))
             data.extend(game_week_data)
 
     return data
 
 
-def insert_into_db(data, sql_file_path='../sql/insert_fantasy_lord.sql'):
-    print('Shipping data to db')
+def insert_into_db(data, sql_file_path="../sql/insert_fantasy_lord.sql"):
+    print("Shipping data to db")
     query_db = QueryDatabase(db_config)
     query_db.db_insert(sql_file_path, data)
 
@@ -68,5 +112,5 @@ def main():
     insert_into_db(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
